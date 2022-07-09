b5adb9e7022380513c73ff015b7c57dc30254194
Pedro Henrique Barbiero <pedrohenriquebarbiero@hotmail.com>
2022-07-08 23:16:32 -0400

    add wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,14 @@
 Calculator functinos
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
+
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
+
     return x - y
